509426e017292695851b26d5f7a1040a120e1b30
Przemysław Sz <you@example.com>
2021-11-27 19:01:19 +0100

    Add media queries for mobile

--- .history/src/components/BodieRecord_20211127185658.js
+++ .history/src/components/BodieRecord_20211127185658.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BodieRecord(props) {
     return (
         <div className="record">
-            <p >{props.data.name}</p>
+            <p className="bodie-data">{props.data.name}</p>
             <p className="bodie-data">Gravity: {props.data.gravity}</p>
             <p className="bodie-data">Density: {props.data.density}</p>
             <p className="bodie-data">Mass: {props.data.mass ? props.data.mass.massValue : 'NA'}</p>
